f550134cab2e574a6ae031d9a9fcc9be34e97696
JacobMuncy <jacobmuncy0@gmail.com>
2015-12-14 00:50:33 -0500

    Add the verbs array to the English Directory.
    In order for the AI engine to be able to tell more about a noun. It
    needs to know it's actions. Therefore it needs to know verbs.

--- Headers/Library/English/English Directory.h
+++ Headers/Library/English/English Directory.h
@@ -13,3 +13,20 @@ std::string ABC_Uppercase_CALL = "A B C D E F G H I J K L M N O P Q R S T U V W 
 std::string ABC_Lowercase_CALL = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
 
 //________________________________ End Of Alphabet ____________________________________//
+
+
+
+
+
+//------------------------------------ VERBS -----------------------------------------//
+
+std::string VERBS[] = { "Running","Sprinting" };  //--- More Verbs Needed.
+
+//_________________________________ End Of VERBS _____________________________________//
+
+
+
+
+
+
+
