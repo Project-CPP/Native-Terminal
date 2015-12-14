5380ace059b5263938ec0ae46ebf969c6ca228d0
JacobMuncy <jacobmuncy0@gmail.com>
2015-12-14 00:39:18 -0500

    Add the alphabet to the English Directory.
    The alphabet won't be used for much in the program. But there are a few
    reasons the AI engine needs it.

--- Headers/Library/English/English Directory.h
+++ Headers/Library/English/English Directory.h
@@ -3,3 +3,13 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
+
+
+
+
+//----------------------------------- Alphabet ----------------------------------------//
+int AlphabetTotal = 26;
+std::string ABC_Uppercase_CALL = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+std::string ABC_Lowercase_CALL = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+
+//________________________________ End Of Alphabet ____________________________________//
